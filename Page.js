b4febe799e60af0dc4f9218e5d13c67b49d36738
Dhvanee Mehta <33324310+DhvaneeMehta@users.noreply.github.com>
2019-03-27 13:10:01 +0530

    Temp

---
+++ Page.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import firebase from "firebase"
+import Header from './Header'
+
+class Page extends Component {
+    handle
+    render() {
+      return (
+        <div className="page">
+          < Header />
+        </div>
+      );
+    }
+  }
+
+  export default Page
